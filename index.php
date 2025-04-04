36afc6b20ad4bc1ada90e781716f8e1bfe3e93f4
Eduardo Baroli Souza <eduardo_Baroli@hotmail.com>
2025-04-04 15:06:50 -0300

    Adicionando estrutura para o site

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Larissa e Hebrom</title>
+  <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <header>
+
+      <section id="inicio">
+        <img id = "Fotoinicio" src="" alt="">
+        <img id = "Fotoinicio" src="" alt="">
+
+      </section>
+
+
+      <nav>
+
+      </nav>
+      
+    </header>
+    <main>
+
+
+      <section id = "primeira_sessao">
+        <section id = "cronograma">
+
+        </section>
+      </section>
+      
+
+    </main>
+    <footer>
+
+    </footer>
+    <script src="script/codigo.js"></script>
+</body>
+</html>
