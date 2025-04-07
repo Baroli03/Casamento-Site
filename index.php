e305551438cbed127c479b8d958f2e4ef09aa4d5
EDUARDO BAROLI SOUZA <eduardo.souza999@cs.up.edu.br>
2025-04-07 11:16:59 -0300

    Criando design

--- index.php
+++ index.php
@@ -8,29 +8,29 @@
 </head>
 <body>
     <header>
-
-      <section id="inicio">
-        <img id = "Fotoinicio" src="" alt="">
-        <img id = "Fotoinicio" src="" alt="">
-
-      </section>
-
-
-      <nav>
-
-      </nav>
-      
+      <section id="cabecalho">
+        <img id= "logo"  src="img/logo.png" alt="Logo Larissa e Hebrom">
+  </section>      
     </header>
     <main>
+      <h1>
+        <span id="tamanho1"><span id="letra">L</span>ARISSA</span> <br>
+        <span id="tamanho2">and</span>
+        <span id="tamanho1"><span id="letra">H</span>EBROM</span>
+      </h1>
+      <p>PARA NOSSA CELEBRAÇÃO DE CASAMENTO</p>
 
-
-      <section id = "primeira_sessao">
-        <section id = "cronograma">
-
-        </section>
+      <div id="local">
+        <div class="esquerda"><p>JANEIRO, 24<br>2026</p></div>
+        <div id="corte"></div>
+        <div class="direita"><p>CHÁCARA <br>KRAZK</p></div>
+      </div>
+      <section id="rsvp">
+        <p>RSVP</p>
       </section>
-      
+      <section id="imagens">
 
+      </section>
     </main>
     <footer>
 
